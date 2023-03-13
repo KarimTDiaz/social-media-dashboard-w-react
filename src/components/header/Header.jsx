af8244f16bf444f48597a78f34f19756c8beafeb
AlejandroJimenezMurciano <karimtilouni94@gmail.com>
2023-03-13 12:10:50 +0100

    social media dashboard

--- src/components/header/Header.jsx
+++ src/components/header/Header.jsx
@@ -12,21 +12,21 @@ const Header = ({ dark, setDark }) => {
 	return (
 		<StyledHeader dark={dark}>
 			<HeaderTitle dark={dark}>Social Media Dashboard</HeaderTitle>
-			<HeaderInfo size='1rem'>Total Followers: {amount.toLocaleString('de-DE')}</HeaderInfo>
+			<HeaderInfo size='1rem'>
+				Total Followers: {totalFollowers().toLocaleString('de-DE')}
+			</HeaderInfo>
 			<HeaderToggleContainer>
-				<HeaderInfo size='0.9rem'>{dark ? 'Dark Mode' : 'Light Mode'}</HeaderInfo>
+				<HeaderInfo size='0.9rem'>
+					{dark ? 'Dark Mode' : 'Light Mode'}
+				</HeaderInfo>
 				<Toggle dark={dark} setDark={setDark} />
 			</HeaderToggleContainer>
 		</StyledHeader>
 	);
 };
-let amount =0;
 
 const totalFollowers = () => {
-	BIG_CARDS_INFO.forEach(card => {
-		amount += card.number
-	})
-}
-totalFollowers()
+	return BIG_CARDS_INFO.reduce((acc, current) => acc + current.number, 0);
+};
 
 export default Header;
